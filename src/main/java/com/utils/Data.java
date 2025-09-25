bfb549843bc18799424e130ed949f96806f340bd
MarySnik <mary.snikk@gmail.com>
2025-09-25 23:01:55 +0800

    new

---
+++ src/main/java/com/utils/Data.java
@@ -0,0 +1,10 @@
+package com.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Data {
+    public static List<MonthReport> monthReports = new ArrayList<>();
+    public static List<YearReport> yearReports = new ArrayList<>();
+
+}
